d611c4b62b8068b5489d9a867d877465242bc36e
n01293036@humbermail.ca <Canhduc1>
2020-09-23 10:22:01 -0400

    Fix something error

---
+++ DucNguyen_CENG319Lab1_Ex1/build/generated/source/buildConfig/debug/assignment1/example/ducnguyen_ceng319lab1_ex1/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package assignment1.example.ducnguyen_ceng319lab1_ex1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "assignment1.example.ducnguyen_ceng319lab1_ex1";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
